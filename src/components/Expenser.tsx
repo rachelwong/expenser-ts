0fac0a631fd6516eb706931ee8cdf08dfb3bc156
rachel-wong <hello.rachelwong@gmail.com>
2023-07-24 00:15:47 +1000

    feat: added shoddy search, filter, toolbar

--- src/components/Expenser.tsx
+++ src/components/Expenser.tsx
@@ -1,23 +1,53 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, ChangeEvent } from 'react'
 import { useExpenseContext } from '../context/ExpenseContext'
 import baseExpenses from '../data/baseExpenses.json'
-import { Stack, Row, Col, Button, Container } from 'react-bootstrap'
+import { Stack, Row, Col, Button, Container, Form } from 'react-bootstrap'
 import ExpenseItem from './ExpenseItem'
 import MonthCapForm from './MonthCapForm'
 import Field from './Field'
+import { debounce } from "lodash"
+import { IExpenseItem } from '../interfaces/IExpense'
 
 const Expenser = () => {
   const { expenseItems, setExpenseItems, monthCap, setMaxExpenses, openModal, clearExpenses, getTotalExpenses } = useExpenseContext();
   const [monthCapFormStatus, setMonthCapFormStatus] = useState<boolean>(false)
+  const [showToolbar, setShowToolbar] = useState<boolean>(false)
+  const [query, setQuery] = useState<string>('')
+  const [sortAsc, setSortAsc] = useState<boolean>(false)
+  const [filteredExpenses, setFilteredExpenses] = useState<IExpenseItem[]>(expenseItems)
+
+  const handleQuery = (evt: ChangeEvent<HTMLInputElement>): void => {
+    setQuery((evt.target.value).trim().toLowerCase())
+  }
+  const debouncedQuery = debounce(handleQuery, 500);
 
   useEffect(() => {
     if (!expenseItems || !expenseItems.length) setExpenseItems(baseExpenses)
     if (!monthCap) setMonthCapFormStatus(true)
-  }, [expenseItems, setExpenseItems, monthCap])
+    if (query) {
+      setFilteredExpenses((prev) => {
+        return expenseItems.filter(item =>
+          item.name.toLowerCase().search(query) > -1 || item.comment.toLowerCase().search(query) > -1)
+      })
+    } else {
+      setFilteredExpenses([...expenseItems])
+    }
+    setFilteredExpenses((prev) => {
+      if (sortAsc) {
+        return prev.sort((a, b) => {
+          return (a.name < b.name) ? 1 : (a.name > b.name) ? -1 : 0;
+        })
+      } else {
+        return prev.sort((a, b) => {
+          return (a.name < b.name) ? -1 : (a.name > b.name) ? 1 : 0;
+        })
+      }
+    })
+  }, [expenseItems, setExpenseItems, monthCap, query, sortAsc])
 
   return (
     <Container>
-      <MonthCapForm status={monthCapFormStatus} setStatus={ setMonthCapFormStatus } />
+      <MonthCapForm status={monthCapFormStatus} setStatus={setMonthCapFormStatus} />
       <Stack>
         <Row>
           <Col>
@@ -31,16 +61,28 @@ const Expenser = () => {
             </h3>
           </Col>
           <Col className="d-flex justify-content-end">
+            <Button variant="outline-info" onClick={() => setShowToolbar(!showToolbar)}>
+              {showToolbar && 'Hide '}Options
+            </Button>
             <Button variant="secondary" onClick={ clearExpenses}>Clear All</Button>
             <Button variant="primary" onClick={openModal}>+ New</Button>
           </Col>
         </Row>
+        {showToolbar &&
+          <Stack direction="horizontal">
+            <Button onClick={ () => setSortAsc(!sortAsc) }>Sort { sortAsc ? 'A-Z' : 'Z-A' }</Button>
+            <Form>
+              <Form.Group>
+                <Form.Control className="mx-1" placeholder="Search Expenses" type='text' name="search" onChange={debouncedQuery} />
+              </Form.Group>
+            </Form>
+        </Stack>}
         {!expenseItems.length && (
           <Row className="d-flex justify-content-center align-items-center">
-            <h3>No expenses recorded</h3>
+            <h3>{ !query ? 'No expenses recorded' : `No results for ${query}`}</h3>
           </Row>
         )}
-        {expenseItems.length && (
+        {filteredExpenses.length && (
           <>
             <Row className='w-100'>
               <Col xs={ 2} className="text-center">Name</Col>
@@ -50,7 +92,7 @@ const Expenser = () => {
               <Col xs={2} className="text-center">Amount</Col>
               <Col xs={1 } className="text-center">Actions</Col>
             </Row>
-            {expenseItems.map((item) => (
+            {filteredExpenses.map((item) => (
               <ExpenseItem expenseItem={item} key={ item.id } />
             ))}
             <Row className='w-100'>
